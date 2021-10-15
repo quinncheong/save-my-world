01ea02a5a9e918a9aa4e4ee3dcacfd2df81e20ec
Wong Jie Peng <76239879+jiepengwong@users.noreply.github.com>
2021-10-16 01:32:38 +0800

    Added potential globe with a simple login

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,34 +1,61 @@
 <template>
-<header class='page-header'>
   <div class="container-fluid ">
 <!-- This would be where the globe would be  -->
-    <div class="row  py-5" id='login'>
+    <div class="row mt-3" id='login'>
       <div class="col">
-        Earth globe thingy goes here (Not that important yet)
+        <img src="@/assets/img/globe.png" alt="searchicon" >
       </div>
+      <!-- Login section -->
 
-      <div class="col text-start">
-        <h1 class='mt-3 text-center'>You have to power to <span id='savemyworld'>#SaveOurWorld</span></h1>
-
-        <div class="login mt-3 ">
-          <div class="mb-3">
-            <label for="password" class="form-label">Username</label>
-            <input type="email" class="form-control" placeholder="Username">
+      <!-- Tile of login  -->
+      <div class="col text-star">
+        <div class="row">
+          <div class="col">
+            <h1 class='mt-3 text-center'>You have to power to <span id='savemyworld'>#SaveOurWorld</span></h1>
           </div>
+        
+        <!-- Row 2 username -->
+        <div class="row  my-4">
+          <div class="col">
+            <div class="centered">
+                <input type="password" class="centered form-control w-50"  placeholder="Username">  
+            </div>
+          </div>
+        </div>
 
-          <div class="mb-3">
-            <label for="password" class="form-label">Password</label>
-            <input type="email" class="form-control" id='password'  placeholder="Password">
+        <!-- Row 3 password -->
+
+        <div class="row justify-content-center">
+          <div class="centered col ">
+              <input type="password" class="form-control w-50 "  placeholder="Password">
+          </div>  
+        </div>
+
+
+        <!-- Row 4 button -->
+
+        <div class="row">
+
+          <div class="centered my-4">
+            <button class='btn btn-primary w-50'>Login</button>
           </div>
 
         </div>
+
+      </div>
+        
+        
+        
+        
       </div>
     </div>
   </div>
-</header>
 
 
-  <!-- Guides -->
+<!-- Guides -->
+
+
+
     
 
 
@@ -36,7 +63,7 @@
 
   
 
-    <!-- Footer goes here -->
+   
 
 
 
@@ -46,13 +73,22 @@
 
 <script>
 // @ is an alias to /src
+import Globe from "@/assets/img/globe.png";
 
 export default {
+  name: "Home",
+  data() {
+    return {
+      Globe
+    }
+  }
   
 };
 </script>
 
 <style lang="scss">
+
+
 .gg {
   color: $testing-variable;
 }
@@ -60,6 +96,16 @@ export default {
 #savemyworld{
   color: yellow;
 }
+
+
+.centered {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+
+
 
 
 </style>
