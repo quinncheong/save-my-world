6dbb64b2d70fbbfbed21cd2b0cb77d468955708d
Quinn Cheong <quinncheong@hotmail.com>
2021-10-17 19:36:23 +0800

    Responsive navbar

--- src/components/Layout/Navbar.vue
+++ src/components/Layout/Navbar.vue
@@ -1,5 +1,5 @@
 <template>
-  <nav id="nav" v-if="windowWidth > 800">
+  <nav class="nav" v-if="windowWidth > 800">
     <!-- <router-link id="home" to="/">Home img</router-link> -->
     <router-link id="home" to="/">
       <img class="homeimg" src="@/assets/img/world.png" alt="Globe Icon" />
@@ -10,19 +10,21 @@
     <router-link to="/profile">Profile</router-link>
   </nav>
 
-  <nav id="nav" v-else>
+  <nav class="nav small" v-else>
     <!-- <router-link id="home" to="/">Home img</router-link> -->
-    <router-link id="home" to="/">
-      <img class="homeimg" src="@/assets/img/world.png" alt="Globe Icon" />
-    </router-link>
-    <div class="nav-dropdown">
-      <button class="nav-dropdown-button btn btn-success">Dropdown</button>
-      <div class="dropdown-content dropleft">
-        <router-link to="/news">News</router-link>
-        <router-link to="/community">Community</router-link>
-        <router-link to="/login">Login</router-link>
-        <router-link to="/profile">Profile</router-link>
-      </div>
+    <div class="top">
+      <router-link id="home" to="/">
+        <img class="homeimg" src="@/assets/img/world.png" alt="Globe Icon" />
+      </router-link>
+      <button @click="showButton()" class="nav-dropdown-button btn btn-success">
+        Dropdown
+      </button>
+    </div>
+    <div :class="dropdownClass">
+      <router-link to="/news">News</router-link>
+      <router-link to="/community">Community</router-link>
+      <router-link to="/login">Login</router-link>
+      <router-link to="/profile">Profile</router-link>
     </div>
   </nav>
 </template>
@@ -33,19 +35,37 @@ export default {
   data() {
     return {
       windowWidth: 0,
+      dropdownClass: "dropdown-content",
+      dropdownShown: false,
     };
+  },
+  methods: {
+    showButton() {
+      if (this.dropdownShown) {
+        this.dropdownShown = !this.dropdownShown;
+        this.dropdownClass = "dropdown-content";
+      } else {
+        this.dropdownShown = !this.dropdownShown;
+        this.dropdownClass = "dropdown-content show-content";
+      }
+    },
   },
   mounted() {
     //   track the width on resize
     window.addEventListener("resize", () => {
       this.windowWidth = window.innerWidth;
+      if (window.innerWidth > 800) {
+        //   reset it if you expand the screen
+        this.dropdownShown = false;
+        this.dropdownClass = "dropdown-content";
+      }
     });
   },
 };
 </script>
 
 <style lang="scss" scoped>
-#nav {
+.nav {
   padding: 30px;
   background-color: $bg-color-primary;
   display: flex;
@@ -53,7 +73,6 @@ export default {
 
   #home {
     margin-right: auto;
-
     .homeimg {
       border-radius: 50%;
       height: 30px;
@@ -78,40 +97,53 @@ export default {
       color: $font-color-secondary;
     }
   }
-}
 
-// For dropdown
-.nav-dropdown {
-  .dropdown-button {
-    font-size: $font-size-primary;
-    background-color: inherit;
-    font-family: inherit; /* Important for vertical align on mobile phones */
-    margin: 0; /* Important for vertical align on mobile phones */
-  }
+  &.small {
+    flex-direction: column;
+    align-items: space-around;
+    position: relative;
 
-  .dropdown-content {
-    display: none;
-    position: absolute;
-    background-color: $bg-color-primary;
-    // min-width: 160px;
-    box-shadow: 0px 8px 16px 0px rgba(0, 0, 0, 0.2);
-    z-index: 1;
+    .top {
+      display: flex;
+      align-content: center;
 
-    a {
-      color: black;
-      padding: 12px 16px;
-      text-decoration: none;
-      display: block;
-      text-align: left;
-
-      &:hover {
-        background-color: $bg-color-primary;
+      // For dropdown
+      .dropdown-button {
+        font-size: $font-size-primary;
+        background-color: inherit;
+        font-family: inherit; /* Important for vertical align on mobile phones */
+        margin: 0; /* Important for vertical align on mobile phones */
       }
     }
-  }
 
-  &:hover .dropdown-content {
-    display: block;
+    .dropdown-content {
+      position: absolute;
+      top: 80px;
+      right: 30px;
+      display: none;
+      flex-direction: column;
+      background-color: $bg-color-secondary;
+      box-shadow: 0px 8px 16px 0px rgba(0, 0, 0, 0.2);
+      z-index: 1;
+
+      &.show-content {
+        display: flex;
+      }
+
+      a {
+        color: white;
+        padding: 12px 16px;
+        margin-left: 0px;
+        margin-right: 0px;
+        text-decoration: none;
+        display: flex;
+        text-align: left;
+
+        &:hover {
+          background-color: $testing-variable;
+        }
+      }
+    }
   }
 }
 </style>
