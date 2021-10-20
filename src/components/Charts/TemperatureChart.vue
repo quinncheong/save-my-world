e8ae0fbeb82dcac5f55f3a6a68c8faf7a21c768a
Quinn Cheong <quinncheong@hotmail.com>
2021-10-20 21:20:15 +0800

    Migrate to composition api for chart

--- src/components/Charts/TemperatureChart.vue
+++ src/components/Charts/TemperatureChart.vue
@@ -1,23 +1,29 @@
 <template>
-  <div style="height:600px;width: 600px; display: flex;flex-direction:column;">
+  <div style="height:600px;width: 600px;display: flex;flex-direction:column;">
     <vue3-chart-js
         :id="doughnutChart.id"
+        ref="chartRef"
         :type="doughnutChart.type"
         :data="doughnutChart.data"
-        @before-render="beforeRenderLogic"
+        :options="doughnutChart.options"
     ></vue3-chart-js>
+
+    <button @click="updateChart">Update Chart</button>
   </div>
 </template>
 
 <script>
+import { ref } from 'vue'
 import Vue3ChartJs from '@j-t-mcc/vue3-chartjs'
 
 export default {
-  name: 'TempChart',
+  name: 'App',
   components: {
     Vue3ChartJs,
   },
   setup () {
+    const chartRef = ref(null)
+
     const doughnutChart = {
       id: 'doughnut',
       type: 'doughnut',
@@ -34,20 +40,83 @@ export default {
             data: [40, 20, 80, 10]
           }
         ]
+      },
+      options: {
+        plugins: {}
       }
     }
 
-    const beforeRenderLogic = (event) => {
-      //...
-      //if(a === b) {
-      //  event.preventDefault()
-      //}
+    const updateChart = () => {
+      doughnutChart.options.plugins.title = {
+        text: 'Updated Chart',
+        display: true
+      }
+      doughnutChart.data.labels = ['Cats', 'Dogs', 'Hamsters', 'Dragons']
+      doughnutChart.data.datasets = [
+        {
+          backgroundColor: [
+            '#333333',
+            '#E46651',
+            '#00D8FF',
+            '#DD1B16'
+          ],
+          data: [100, 20, 800, 20]
+        }
+      ]
+
+      chartRef.value.update(null)
     }
 
     return {
       doughnutChart,
-      beforeRenderLogic
+      updateChart,
+      chartRef
     }
   },
 }
+
+// Old options api
+// export default {
+//   name: "TempChart",
+//   components: {
+//     Vue3ChartJs,
+//   },
+//   data() {
+//     return {
+//       chartId: "doughnut",
+//       chartType: "doughnut",
+//       labels: ["vvvvv", "EmberJs", "ReactJs", "AngularJs"],
+//       datasets: [
+//         {
+//           backgroundColor: ["#41B883", "#E46651", "#00D8FF", "#DD1B16"],
+//           data: [40, 20, 80, 10],
+//         },
+//       ],
+//       options: {
+//         plugins: {}
+//       }
+//     };
+//   },
+//   methods: {
+//     changeData() {
+//       console.log("inside the change");
+//       this.labels = ["VueeeeeJs", "EmberJs", "ReactJs", "AngularJs"];
+//       this.datasets = [
+//         {
+//           backgroundColor: ["#41B883", "#E46651", "#00D8FF", "#DD1B16"],
+//           data: [10, 20, 80, 10],
+//         }
+//       ]
+//     },
+//   },
+//   computed: {
+//     getData() {
+//       let options = {
+//         plugins: {}
+//       }
+//       let data = { labels: this.labels, datasets: this.datasets, options };
+//       return data
+//     },
+//   },
+// };
 </script>
