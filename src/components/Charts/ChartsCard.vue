1d29cb825e0bf35b331ab5d4e8461201158b5101
Quinn Cheong <quinncheong@hotmail.com>
2021-10-20 12:21:50 +0800

    Final responsive for charts

--- src/components/Charts/ChartsCard.vue
+++ src/components/Charts/ChartsCard.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="card-bg col-12 col-sm-5 col-md-3">
+  <div class="card-bg col-5 col-md-3">
     <img src="@/assets/img/globe.png" alt="" />
     <p class="card-title">
       {{ name }}
@@ -24,7 +24,7 @@ export default {
   background-color: $tertiary-color-opacity;
   display: flex;
   flex-direction: column;
-  padding: 1rem;
+  padding: 0.5em;
   border: 1px solid white;
   margin-bottom: 7%;
 
@@ -40,11 +40,9 @@ export default {
   }
 }
 
-
-@media screen and (max-width: 580px) {
-    .card-bg {
-        width: 60%;
-        font-size: 16px;
-    }
+@media screen and (max-width: 380px) {
+  .card-bg {
+    font-size: 11px;
+  }
 }
 </style>
