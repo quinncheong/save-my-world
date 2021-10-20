b435de39eefcfc0e8ff5a912ecdeba99283f8a99
Quinn Cheong <quinncheong@hotmail.com>
2021-10-20 22:06:16 +0800

    Install relevant libraries and connect to API

--- src/components/Charts/TemperatureChart.vue
+++ src/components/Charts/TemperatureChart.vue
@@ -1,79 +1,122 @@
 <template>
-  <div style="height:600px;width: 600px;display: flex;flex-direction:column;">
-    <vue3-chart-js
-        :id="doughnutChart.id"
-        ref="chartRef"
-        :type="doughnutChart.type"
-        :data="doughnutChart.data"
-        :options="doughnutChart.options"
-    ></vue3-chart-js>
+  <div
+    style="height: 600px; width: 600px; display: flex; flex-direction: column"
+  >
+    <form @submit="handleClick">
+      <label>
+        Select a country:
+        <select v-model="country" >
+          <option :key="country.name" v-for="country in countries" :value="country.name">{{country.name}}</option>
+        </select>
+      </label>
 
-    <button @click="updateChart">Update Chart</button>
+      <label>
+        Select a year (must be multiples of 20):
+        <input v-model="year" type="number" required />
+      </label>
+      <button >Get Data</button>
+    </form>
+
+    <vue3-chart-js
+      :id="doughnutChart.id"
+      ref="chartRef"
+      :type="doughnutChart.type"
+      :data="doughnutChart.data"
+      :options="doughnutChart.options"
+    />
+
   </div>
 </template>
 
 <script>
-import { ref } from 'vue'
-import Vue3ChartJs from '@j-t-mcc/vue3-chartjs'
+import { ref } from "vue";
+import Vue3ChartJs from "@j-t-mcc/vue3-chartjs";
+import axios from "axios";
+import { countries } from "countries-list";
+import iso from "iso-3166-1"; // Library to key in country name and get iso no.
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
     Vue3ChartJs,
   },
-  setup () {
-    const chartRef = ref(null)
+  setup() {
+    const chartRef = ref(null);
+    const country = ref("");
+    const year = ref(2000);
 
     const doughnutChart = {
-      id: 'doughnut',
-      type: 'doughnut',
+      id: "doughnut",
+      type: "doughnut",
       data: {
-        labels: ['VueJs', 'EmberJs', 'ReactJs', 'AngularJs'],
+        labels: [""],
         datasets: [
           {
             backgroundColor: [
-              '#41B883',
-              '#E46651',
-              '#00D8FF',
-              '#DD1B16'
+              "#41B883",
+              // '#E46651',
+              // '#00D8FF',
+              // '#DD1B16'
             ],
-            data: [40, 20, 80, 10]
-          }
-        ]
+            data: [40, 20, 80, 10],
+          },
+        ],
       },
       options: {
-        plugins: {}
+        plugins: {},
+      },
+    };
+
+    // Function to load the new data
+    const handleClick = async (e) => {
+      e.preventDefault();
+      let baseUrl =
+        "http://climatedataapi.worldbank.org/climateweb/rest/v1/country/annualavg/tas";
+      let isoCountry = iso.whereCountry(country.value).alpha3;
+      let startYear = year.value;
+      let endYear = startYear + 19;
+      let fullUrl = `${baseUrl}/${startYear}/${endYear}/${isoCountry}`;
+      console.log(fullUrl);
+      
+      try {
+        let res = await axios.get(fullUrl);
+        if (!res) {
+          throw Error("Error in accessing API");
+        }
+
+        console.log(res.data);
+      } catch (error) {
+        console.log(error);
       }
-    }
+    };
 
     const updateChart = () => {
       doughnutChart.options.plugins.title = {
-        text: 'Updated Chart',
-        display: true
-      }
-      doughnutChart.data.labels = ['Cats', 'Dogs', 'Hamsters', 'Dragons']
+        text: "Updated no Chart",
+        display: true,
+      };
+      doughnutChart.data.labels = ["Cats", "Dogs", "Hamsters", "Dragons"];
       doughnutChart.data.datasets = [
         {
-          backgroundColor: [
-            '#333333',
-            '#E46651',
-            '#00D8FF',
-            '#DD1B16'
-          ],
-          data: [100, 20, 800, 20]
-        }
-      ]
+          backgroundColor: ["#333333", "#E46651", "#00D8FF", "#DD1B16"],
+          data: [100, 20, 800, 20],
+        },
+      ];
 
-      chartRef.value.update(null)
-    }
+      chartRef.value.update(null);
+    };
 
     return {
       doughnutChart,
+      chartRef,
+      year,
+      country,
       updateChart,
-      chartRef
-    }
+      handleClick,
+      countries,
+    };
   },
-}
+};
 
 // Old options api
 // export default {
