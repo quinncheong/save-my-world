d211df6ed7f85955bead83b04950e0a3cbe8b1d3
asianburpgirl <54923001+asianburpgirl@users.noreply.github.com>
2021-10-22 18:07:37 +0800

    Zq branch (#14)
    * Add prettier and scss
    
    * Change remove vue.config
    
    * Change prettier and scss
    
    * testing branch 1
    
    * Implement prelim navbar routing
    
    * Create community page
    
    * Add theme and searchbar
    
    * testing branch 1 (#7)
    
    Co-authored-by: Wong Jie Peng <76239879+jiepengwong@users.noreply.github.com>
    
    * testing commit
    
    * removal of some text
    
    * Adding of bootstrap CDN and setting up of home.vue
    
    * changing to bootstrap
    
    * Creation of login and globe
    
    * edited campaigns, searchbar and about page, added in images under assets
    
    * added in comments for the changes i made in campaigns, searchbar and about pages
    
    * Added potential globe with a simple login
    
    * Added various pictures for the guides, as well as implementing the guide section. The actual content is to be decided
    
    * Added some comments , pls take note that  this is not responsive yet and i might need help with some CSS stuff. Thanks yous
    
    * updated the home icon but need some fixs
    
    * try out commit
    
    * changing of login text
    
    * testing changes
    
    * Added a profile.vue page, and added the router links for profile tab
    
    * Added small profile card for users to input their status and profile pic (1st Row)
    
    * Account Settings (1st Row)
    
    * Badges and Campaigns (2nd Row)
    
    * Donation History (2nd Row)
    
    * Prelim addition to scss
    
    * Initial changes
    
    * Fix router path
    
    * Decouple navbar
    
    * Create dropdown bar on small screeens
    
    * Responsive navbar
    
    * Edited responsive navbar
    
    * Edit navbar responsive code
    
    * Decouple Campaigns
    
    * Add campaign card code
    
    * Add in navigation
    
    * Added in visualizeation component
    
    * added mapbox cdn to index.html
    
    * Move Layout directory
    
    * updated mapbox gl in index.html
    
    * test
    
    * added mapbox GL js and mapbox-gl-coder
    
    * Add changes to route
    
    * Add wrapper css for app children
    
    * Initial responsive layout for charts
    
    * Add variable font and charts card
    
    * Add responsive to charts
    
    * Final responsive for charts
    
    * Introduce correct chartjs library
    
    * Migrate to composition api for chart
    
    * Install relevant libraries and connect to API
    
    * Touchup css
    
    * Updated temperature api and connect
    
    * Fix merge conflict bugs
    
    * Initial changes to airpolution chart
    
    * Created map with search on Chart.vue
    
    * Add geo and weather apis
    
    * Revert "Created map with search on Chart.vue"
    
    This reverts commit e8699e3928044839c45744faa13fda9aa5c70057.
    
    * implementation of map using mapbox gl js
    
    * edited id of app to map typo
    
    * Add airpollution chart initial
    
    * Add in dyamic rendering for chart component
    
    * Created a new view for the Image Slider
    
    * Change package-lock json
    
    Co-authored-by: Quinn Cheong <quinncheong@hotmail.com>
    Co-authored-by: Wong Jie Peng <76239879+jiepengwong@users.noreply.github.com>
    Co-authored-by: Quinn Cheong <58715320+quinncheong@users.noreply.github.com>
    Co-authored-by: WeiTingsys <lauweiting2012@gmail.com>
    Co-authored-by: WeiTingsys <76719595+WeiTingsys@users.noreply.github.com>

---
+++ src/views/ImageSlide.vue
@@ -0,0 +1,89 @@
+<template>
+  <div class="community">
+    <!-- header -->
+    <h1>Look at the current live situation</h1>
+    <!-- <div class="row">
+      <div class="col-sm-6">
+        <h1>Do what you can for the community</h1>
+      </div>
+      <div class="col-sm-6">
+        <img src="@/assets/img/community.png" alt="" />
+      </div>
+    </div> -->
+    <!-- line breaking the header and the seachbar + campaigns -->
+    <div class="img-comp-container">
+      <div class="img-comp-img">
+        <img src="/assets/img/clothes.png" width="300" height="200">
+      </div>
+      <div class="img-comp-img img-comp-overlay">
+        <img src="/assets/img/clothes.png" width="300" height="200">
+      </div>
+    </div>
+    <ImgComparisonSlider>
+    <!-- eslint-disable -->
+    <img
+      slot="first"
+      style="width: 100%"
+      src="https://img-comparison-slider.sneas.io/demo/images/before.webp"
+    />
+    <img
+      slot="second"
+      style="width: 100%"
+      src="https://img-comparison-slider.sneas.io/demo/images/after.webp"
+    />
+    <!-- eslint-enable -->
+  </ImgComparisonSlider>
+  </div>
+</template>
+
+<script >
+export default {
+  name: "ImageSlider",
+  components: {
+  },
+  methods: {}
+};
+</script>
+
+<style lang="scss" scoped>
+.community {
+  display: flex;
+  flex-direction: column;
+}
+
+hr {
+  border: solid 2px white;
+}
+
+* {box-sizing: border-box;}
+
+.img-comp-container {
+  position: relative;
+  height: 200px; /*should be the same height as the images*/
+}
+
+.img-comp-img {
+  position: absolute;
+  width: auto;
+  height: auto;
+  overflow: hidden;
+}
+
+.img-comp-img img {
+  display: block;
+  vertical-align: middle;
+}
+
+.img-comp-slider {
+  position: absolute;
+  z-index: 9;
+  cursor: ew-resize;
+  /*set the appearance of the slider:*/
+  width: 40px;
+  height: 40px;
+  background-color: #2196F3;
+  opacity: 0.7;
+  border-radius: 50%;
+}
+
+</style>
