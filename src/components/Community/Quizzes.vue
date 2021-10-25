2fd07d0fff31445313410b4a8d4db44969192c8b
asianburpgirl <estherrrlychee@gmail.com>
2021-10-25 18:45:14 +0800

    changed the color of the quiz

--- src/components/Community/Quizzes.vue
+++ src/components/Community/Quizzes.vue
@@ -1,5 +1,5 @@
 <template>
-  <flow-form class="text-white" v-bind:questions="questions" v-bind:language="language" />
+  <flow-form class="text-white" v-bind:questions="questions" />
 </template>
 
 <script>
@@ -34,8 +34,8 @@
   /* Import Vue Flow Form base CSS */
   @import '~@ditdot-dev/vue-flow-form/dist/vue-flow-form.css';
   /* Import one of the Vue Flow Form CSS themes (optional) */
-  @import '~@ditdot-dev/vue-flow-form/dist/vue-flow-form.theme-minimal.css';
-  /* @import '~@ditdot-dev/vue-flow-form/dist/vue-flow-form.theme-green.css'; */
+  /* @import '~@ditdot-dev/vue-flow-form/dist/vue-flow-form.theme-minimal.css'; */
+  @import '~@ditdot-dev/vue-flow-form/dist/vue-flow-form.theme-green.css';
   /* @import '~@ditdot-dev/vue-flow-form/dist/vue-flow-form.theme-purple.css'; */
 
 </style>
