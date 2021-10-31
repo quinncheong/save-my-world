6aee24735d05ae0073493f3b327154fc05033f91
Quinn Cheong <quinncheong@hotmail.com>
2021-10-31 21:28:10 +0800

    Remove community

--- src/components/Community/Quizzes.vue
+++ src/components/Community/Quizzes.vue
@@ -1,66 +1,86 @@
 <template>
-  <flow-form v-bind:questions="questions"/>
+  <div>
+    <div v-if="started">
+      <flow-form class="mt-2" v-bind:questions="questions" />
+    </div>
+    <div v-else>
+      <button class="btn btn-success" @click="startQuiz()">
+        Start Quiz
+      </button>
+    </div>
+  </div>
 </template>
 
 <script>
-  // Import necessary components and classes
-  import { FlowForm, QuestionModel, QuestionType, ChoiceOption} from '@ditdot-dev/vue-flow-form';
-  import {  getQuizzes  } from "@/firebase.js";
+// Import necessary components and classes
+import {
+  FlowForm,
+  QuestionModel,
+  QuestionType,
+  ChoiceOption,
+} from "@ditdot-dev/vue-flow-form";
+import { getQuizzes } from "@/firebase.js";
 
-  var quizzes = getQuizzes();
-  console.log(quizzes)
-//   var responses = quizzes.then(response => {return response})
+export default {
+  name: "Quizzes",
+  components: {
+    FlowForm,
+  },
+  data() {
+    return {
+      questions: [
+        // QuestionModel array
+        new QuestionModel({
+          title: "Question",
+          type: QuestionType.MultipleChoice,
+          options: [
+            new ChoiceOption({
+              label: "Answer",
+            }),
+            new ChoiceOption({
+              label: "no",
+            }),
+          ],
+        }),
+      ],
+      started: false,
+    };
+  },
+  methods: {
+    async startQuiz() {
+      const quizzes = await getQuizzes();
+      this.quiz = quizzes;
 
-  export default {
-    name: 'Quizzes',
-    components: {
-      FlowForm
+
+      this.started = true
     },
-    data() {
-      return {
-        questions: [
-          // QuestionModel array
-          new QuestionModel({
-            title: 'Question',
-            type: QuestionType.MultipleChoice,
-            options: [
-              new ChoiceOption({
-                label: 'Answer'
-              }),
-              new ChoiceOption({
-                label: 'no'
-              })
-            ]
-          })
-        ]
-      }
-    }
-    //   computed: {
-    //       quiz(){
-    //           for(i=0; i<response.length; i++){
-    //               new QuestionModel({
-    //                 title: response.,
-    //                 type: QuestionType.MultipleChoice,
-    //                 options: [
-    //                 new ChoiceOption({
-    //                     label: 'Answer'
-    //                 }),
-    //                 new ChoiceOption({
-    //                     label: 'no'
-    //                 })
-    //                 ]
-    //             })
-    //           }
-    //       }
-    // }
-  }
+  },
+  //   computed: {
+  //       quiz(){
+  //           for(i=0; i<response.length; i++){
+  //               new QuestionModel({
+  //                 title: response.,
+  //                 type: QuestionType.MultipleChoice,
+  //                 options: [
+  //                 new ChoiceOption({
+  //                     label: 'Answer'
+  //                 }),
+  //                 new ChoiceOption({
+  //                     label: 'no'
+  //                 })
+  //                 ]
+  //             })
+  //           }
+  //       }
+  // }
+};
 </script>
 
 <style>
-  /* Import Vue Flow Form base CSS */
-  @import '~@ditdot-dev/vue-flow-form/dist/vue-flow-form.css';
-  /* Import one of the Vue Flow Form CSS themes (optional) */
-  /* @import '~@ditdot-dev/vue-flow-form/dist/vue-flow-form.theme-minimal.css'; */
-  @import '~@ditdot-dev/vue-flow-form/dist/vue-flow-form.theme-green.css';
-  /* @import '~@ditdot-dev/vue-flow-form/dist/vue-flow-form.theme-purple.css'; */
+/* Import Vue Flow Form base CSS */
+@import "~@ditdot-dev/vue-flow-form/dist/vue-flow-form.css";
+/* Import one of the Vue Flow Form CSS themes (optional) */
+/* @import '~@ditdot-dev/vue-flow-form/dist/vue-flow-form.theme-minimal.css'; */
+@import "~@ditdot-dev/vue-flow-form/dist/vue-flow-form.theme-green.css";
+/* @import '~@ditdot-dev/vue-flow-form/dist/vue-flow-form.theme-purple.css'; */
 </style>
