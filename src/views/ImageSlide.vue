8ef0dc39b5b896c06924147fb4a534084dcecabb
asianburpgirl <estherrrlychee@gmail.com>
2021-11-02 18:00:39 +0800

    formatted things in disaster map section

--- src/views/ImageSlide.vue
+++ src/views/ImageSlide.vue
@@ -2,10 +2,18 @@
   <div class="community">
     <!-- header -->
       <div class="row">
-      <div class="col-12"> 
-        <imageSlider/>
-        <br>
-        <div class="jumbotron jumbotron-fluid bg-secondary">
+      <div class="col-4"> 
+        <div class="card text-white bg-secondary" style="width: 18rem;">
+          <br>
+          <imageSlider/>  
+          <div class="card-body">
+            <h5 v-bind:class="card-title">{{cards.title}}</h5>
+                      
+            <a href="#" class="btn btn-primary">Go somewhere</a>
+          </div>
+        </div>
+        
+        <!-- <div class="jumbotron jumbotron-fluid bg-secondary">
           <div class="container">
             <imageCarol/>
             <h1 class="display-">Ice Jam Floods Region in Northwest Alaska</h1>
@@ -14,8 +22,9 @@
                 These false color images show a portion of northwest Alaska before and during widespread flooding in May 2021. Ice and snow appear light blue, liquid water is dark blue or black, and bare ground is yellowish brown. Chunks of ice jammed and blocked the flow of the Buckland River, which then overran its banks. Five feet of water inundated the local community, according to a statement released by Alaska Governor Dunleavy's office.
               </p>
           </div>
-        </div>
+        </div> -->
       </div>
+      <imageSlider/>
     </div>
     <!-- left section things -->
     
@@ -45,6 +54,44 @@ export default {
         width: 800,
         gap: "1rem",
       },
+      cards: [
+        {
+          // text
+          id: 1,
+          title: "ergreeg",
+          text: "rgrgegr"
+        },
+        {
+          id: 2,
+          title: "ergreeg",
+          text: "rgrgegr"
+        },
+        // {
+        //   id: 3,
+        //   img1: "img4A.jpeg", // 4A is wrong and need to change it
+        //   img2: "img4B.jpeg",
+        // },
+        // {
+        //   id: 4,
+        //   img1: "img5A.jpeg",
+        //   img2: "img5B.jpeg",
+        // },
+        // {
+        //   id: 5,
+        //   img1: "img6A.jpeg",
+        //   img2: "img6B.jpeg",
+        // },
+        // {
+        //   id: 6,
+        //   img1: "img7A.jpeg",
+        //   img2: "img7B.jpeg",
+        // },
+        // {
+        //   id: 7,
+        //   img1: "img4A.jpeg",
+        //   img2: "img4B.jpeg",
+        // }
+      ],
     };
   },
 };
