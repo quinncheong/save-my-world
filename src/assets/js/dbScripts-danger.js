607cdeea9df18436d767faa792d87f2054bff759
Quinn Cheong <quinncheong@hotmail.com>
2021-11-02 15:49:24 +0800

    Update quiz component and db script

--- src/assets/js/dbScripts-danger.js
+++ src/assets/js/dbScripts-danger.js
@@ -1,40 +1,38 @@
 // Dangerous file do not touch
-const dbScript = async () => {
-    let file = require('@/assets/data/quiz.json')
-    console.log(file)
-    var main = []
-    let { Quiz1, Quiz2, Quiz3 } = file;
-    
-  
-    for (let quizName in Quiz1) {
-      let final = {
-        id: 1,
-        ...Quiz1[quizName]
-      };
-      main.push(final)
-  
-    }
-  
-    for (let quizName in Quiz2) {
-      let final = {
-        id: 2,
-        ...Quiz1[quizName]
-      };
-      main.push(final)
-    }
-  
-    for (let quizName in Quiz3) {
-      let final = {
-        id: 3,
-        ...Quiz1[quizName]
-      };
-      main.push(final)
-    }
-  
-    // for (let item of main) {
-    //   const docRef = await addDoc(collection(db, "quizzes"), item);
-    // }
-    // console.log(main)
+const dbScript1 = async () => {
+  let file = require("@/assets/data/quiz.json");
+  console.log(file);
+  var main = [];
+  let { Quiz1, Quiz2, Quiz3 } = file;
+
+  for (let quizName in Quiz1) {
+    let final = {
+      quizId: 1,
+      ...Quiz1[quizName],
+    };
+    main.push(final);
   }
-  
-  // dbScript();
+
+  for (let quizName in Quiz2) {
+    let final = {
+      quizId: 2,
+      ...Quiz2[quizName],
+    };
+    main.push(final);
+  }
+
+  for (let quizName in Quiz3) {
+    let final = {
+      quizId: 3,
+      ...Quiz3[quizName],
+    };
+    main.push(final);
+  }
+
+  for (let item of main) {
+    const docRef = await addDoc(collection(db, "quizzes"), item);
+  }
+  console.log(main);
+};
+
+// dbScript1();
