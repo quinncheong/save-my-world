ec6c275ae78d1c74812e59d9fc4bebe1559622b0
asianburpgirl <estherrrlychee@gmail.com>
2021-11-03 01:13:03 +0800

    added in videos

--- src/components/Quiz/Videos.vue
+++ src/components/Quiz/Videos.vue
@@ -2,17 +2,17 @@
     <!-- video -->
     <!-- Grid row -->
     <div class="row">
-        <div class="col-12">
-            <div class="card text-dark">
+        <!-- <div class="col-12">
+            <div class="card border-light text-white">
                 <div class="card-body" style="text-align: left">
                     <p>Gallery of videos</p>
                     <p>To better understand</p>
                 </div>
             </div>
-        </div>
-        <br>
+        </div> -->
+    
         <!-- Grid column -->
-        <div class="col-lg-4 col-md-6 col-sm-12 mb-4">
+        <div class="col-md-6 col-sm-12 mb-4 my-2">
             <div class="embed-responsive embed-responsive-16by9 z-depth-1-half">
                 <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/dcBXmj1nMTQ"
                     allowfullscreen></iframe>
@@ -20,9 +20,41 @@
         </div>
 
         <!-- Grid column -->
-        <div class="col-lg-4 col-md-6 col-sm-12 mb-4">
+        <div class="col-md-6 col-sm-12 mb-4 my-2">
             <div class="embed-responsive embed-responsive-16by9 z-depth-1-half">
                 <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/0Puv0Pss33M"
+                    allowfullscreen></iframe>
+            </div>
+        </div>
+
+        <!-- Grid column -->
+        <div class="col-md-6 col-sm-12 mb-4 my-2">
+            <div class="embed-responsive embed-responsive-16by9 z-depth-1-half">
+                <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/aNX__544IO0"
+                    allowfullscreen></iframe>
+            </div>
+        </div>
+
+        <!-- Grid column -->
+        <div class="col-md-6 col-sm-12 mb-4 my-2">
+            <div class="embed-responsive embed-responsive-16by9 z-depth-1-half">
+                <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/SN5-DnOHQmE"
+                    allowfullscreen></iframe>
+            </div>
+        </div>
+
+        <!-- Grid column -->
+        <div class="col-md-6 col-sm-12 mb-4 my-2">
+            <div class="embed-responsive embed-responsive-16by9 z-depth-1-half">
+                <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/G4H1N_yXBiA"
+                    allowfullscreen></iframe>
+            </div>
+        </div>
+
+        <!-- Grid column -->
+        <div class="col-md-6 col-sm-12 mb-4 my-2">
+            <div class="embed-responsive embed-responsive-16by9 z-depth-1-half">
+                <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/fu5lwMissg0"
                     allowfullscreen></iframe>
             </div>
         </div>
