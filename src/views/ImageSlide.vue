51c3e42733e9c8e0e67b39d2d260af89314ba121
asianburpgirl <estherrrlychee@gmail.com>
2021-11-03 11:08:13 +0800

    deleted the commented out part

--- src/views/ImageSlide.vue
+++ src/views/ImageSlide.vue
@@ -1,19 +1,5 @@
 <template>
   <div class="community">
-    <!-- <div
-      class="container-fluid p-5 text-center"
-    > -->
-    <!-- <div class="mask" style="background-color: rgba(0, 0, 0, 0.6);">
-      <div class="d-flex justify-content-center align-items-center h-100">
-        <div class="text-white">
-          <h1 class="mb-3">Heading</h1>
-          <h4 class="mb-3">Subheading</h4>
-          <a class="btn btn-outline-light btn-lg" href="#!" role="button"
-          >Call to action</a
-          >
-        </div>
-      </div>
-    </div> -->
     <div class="image-change-wrapper">
       <!-- header -->
       <h2 class="chart-title mb-3">Images of the World</h2>
@@ -45,7 +31,7 @@
   <div>
     <image-of-change :images="images" />
   </div>
-<!-- </div> -->
+</div>
 </template>
 
 <script >
