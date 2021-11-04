5eab4625d908899aa00b62ef2dbcf7621dc96968
asianburpgirl <estherrrlychee@gmail.com>
2021-11-04 15:34:31 +0800

    added in new imgs and desc

--- src/views/ImageSlide.vue
+++ src/views/ImageSlide.vue
@@ -77,8 +77,8 @@ export default {
           id: 2,
           img1: "img3A.jpeg",
           img2: "img3B.jpeg",
-          title: "Drought Shrinks California's Shasta Lake",
-          description: "hasta Lake is California's largest reservoir and third largest body of water, a key water source for Central Valley agriculture. But drought has been depleting the lake. Between the times of the two images, Shasta's water level dropped 107 feet (33 meters). Its water storage decreased to about 40% of capacity and about half of the historical average for the time of year shown. The tan fringe around the water in the 2021 image is a “bathtub ring” lakebed that the falling water level exposed."
+          title: "Taal Volcano Ash Coats Philippine Island of Luzon",
+          description: "The Taal volcano, in Taal Lake on the Philippine island of Luzon, erupted on Jan. 12, 2020. During the weeks that followed, wet, heavy ash fell back to Earth, turning the lush green landscape (2019 image) a grayish brown (2020 image). As the ash dried, it hardened from a mud-like consistency to that of cement, damaging most of the island's crops and other vegetation."
         },
         {
           id: 3,
@@ -108,11 +108,132 @@ export default {
           title: "Fast Change in Great Lakes Ice",
           description: "Great Lakes ice cover has declined about 5% each decade since the early 1970s. In keeping with that trend, winter 2020-21 was generally a low-ice season, except for a short period in February, when a brief influx of frigid Arctic air froze nearly half the surface of the lakes. The February image shows the lakes near the peak of the icy blip. The March image shows the scene just 11 days later, when only 15% of the surface was frozen."
         },
-        // {
-        //   id: 7,
-        //   img1: "img4A.jpeg",
-        //   img2: "img4B.jpeg",
-        // }
+        {
+          id: 7,
+          img1: "img8A.jpeg",
+          img2: "img8B.jpeg",
+          title: "Monsoon Flooding in Bangladesh",
+          description: "These images show eastern Bangladesh before and after unusually severe flooding from monsoon rains. According to a report on Aug. 19, 2020, by the United Nations Office for the Coordination of Humanitarian Affairs, flooding in Bangladesh damaged some 1.3 million homes, displaced more than 167,000 families, and affected more than 420,000 acres of agricultural land. In these false-color images, constructed from a combination of visible and infrared light, water appears navy blue and black, clouds are white or cyan, and vegetation is bright green."
+        },
+        {
+          id: 9,
+          img1: "img9A.jpeg",
+          img2: "img9B.jpeg",
+          title: "Lake Victoria's Rising Waters",
+          description: "These images show changes in Winam Gulf, a northeastern extension of Lake Victoria, Africa's largest lake. Months of intense rain raised the water level to the highest point in the three decades of measurement by satellite, putting lakeside communities at risk of flooding. Many of the green patches within the gulf are vegetation. Suspended sediment (particles of soil or other materials) colors some of the water brown."
+        },
+        {
+          id: 10,
+          img1: "img10A.jpeg",
+          img2: "img10B.jpeg",
+          title: "Highs and Lows of China's Poyang Lake",
+          description: "Poyang is China's largest freshwater lake. It typically shrinks during the dry winters and regains its bulk during the summer rains, but it went to extremes in 2019-20. Its water level in December 2019 was the lowest in 60 years, while in July 2020 it was the highest on record. These images are in false color, using a combination of visible and infrared light that clarifies the boundary between water and land. Water appears black and navy blue, vegetation is bright green, and clouds are white or cyan."
+        },
+        {
+          id: 11,
+          img1: "img11A.jpeg",
+          img2: "img11B.jpeg",
+          title: "Flooding in Northeastern China",
+          description: "The 2020 image (right) shows flooding in northeastern China along the Songhua River and its tributaries after a historically rainy monsoon season, capped by persistent rainfall from the remnants of Typhoon Maysak. The image on the left shows the same area a year earlier. In these false-color images, vegetation appears green, water is dark blue, and bare land is brown."
+        },
+        {
+          id: 12,
+          img1: "img12A.jpeg",
+          img2: "img12B.jpeg",
+          title: "India's Lonar Lake Changes Color",
+          description: "Over the span of a few days in June 2020, Lonar Lake in west-central India changed from green to pink. Warm, dry weather had evaporated a significant amount of the lake's water, leaving salts behind. Scientists speculate that with the increased salt concentration, a rapid rise in the numbers of salt-loving microorganisms caused the color change. A similar phenomenon happens periodically in Iran’s Lake Urmia and many Russian lakes when hot weather shrinks the lake and the water grows saltier."
+        },
+        {
+          id: 13,
+          img1: "img13A.jpeg",
+          img2: "img13B.jpeg",
+          title: "\"Great Migration\" Back to Atlanta, Georgia",
+          description: "In a reversal of the \"Great Migration\" north of the early 20th century, people from the Northeast and Midwest have been moving to Atlanta, Georgia, making it one of the fastest growing metropolitan areas in the U.S. These false-color images have been processed to emphasize the contrast between urban and forested or rural areas. Much of the new development visible in the 2020 image is relatively distant from downtown Atlanta."
+        },
+        {
+          id: 14,
+          img1: "img14A.jpeg",
+          img2: "img14B.jpeg",
+          title: "Two Kenyan Lakes Expand, Threatening Both",
+          description: "Torrential rains in Kenya's Great Rift Valley have swollen two important lakes and increased concern that they will merge. These images, in which north is to the left, show the southern end of Lake Baringo (left) and the northern end of Lake Bogoria (dark area at upper right). Both have enlarged significantly in recent years, reducing the distance between them. Baringo contains fresh water, whereas Bogoria, a World Heritage site (along with two other lakes in the same system), is alkaline. Merging their waters would threaten their respective wildlife."
+        },
+        {
+          id: 15,
+          img1: "img15A.jpeg",
+          img2: "img15B.jpeg",
+          title: "Stalled Landslide Threatens Tsunami in Alaskan Fjord",
+          description: "A slow-motion landslide on the slope that defines the western side of Barry Arm, one of the glacial fjords of Alaska's Prince William Sound, has stalled. But scientists warn that the mountainside could fail catastrophically and slide into the fjord, endangering people in the area with a tsunami hundreds of feet high. The slide appears to have begun at least 50 years ago, but sped up between 2009 and 2015 as warming temperatures shortened Barry Arm Glacier, which had supported the slope's base. The mountainside slid about 400 feet (120 meters) between 2010 and 2017, but has moved little since then."
+        },
+        {
+          id: 16,
+          img1: "img16A.jpeg",
+          img2: "img16B.jpeg",
+          title: "Invasion of the Water Hyacinths Across Valsequillo Reservoir, Mexico",
+          description: "These images show the spread of water hyacinths across the Valsequillo reservoir of central Mexico. The floating plants, native to the Amazon basin, are relentless invaders in other parts of the world. They obstruct waterways, clog hydropower plants, and block sunlight from penetrating the water’s surface. Some scientists attribute the problem to water pollution: Nutrients in untreated sewage, which often accompanies rapid urbanization, enable the plants to thrive. In these false-color images, clear water is dark blue and vegetation is red. The brighter the red, the more robust the vegetation."
+        },
+        {
+          id: 17,
+          img1: "img17A.jpeg",
+          img2: "img17B.jpeg",
+          title: "Spring in the Kulunda Steppe, Russia-Kazakhstan Border",
+          description: "This portion of the Kulunda Steppe, which straddles the Russia-Kazakhstan border, displays a common springtime transformation from the white of snow and ice to the green of vegetation. Less common is Lake Kuchukskoye's change from green to pink as rosy, salt-loving organisms in the lake multiplied. Hot weather had increased evaporation and therefore the lake's salt concentration, prompting the population explosion."
+        },
+        {
+          id: 18,
+          img1: "img18A.jpeg",
+          img2: "img18B.jpeg",
+          title: "Sea Ice Declines in Hudson Bay",
+          description: "Hudson Bay, which is considered a sea of the Arctic Ocean despite being surrounded by land, freezes over completely in winter and thaws in summer. These images show the decrease in ice over the course of 16 days in July 2020. During the thaw, polar bears travel over the slowly melting sea ice to hunt for ringed seals and other prey. When the ice becomes too scarce, the bears fast and wait for it to return. The area has lost about a third of the polar bear population since the 1980s, dropping the count from about 1200 to 800, apparently because declining summer sea ice has given them less opportunity to feed."
+        },
+        {
+          id: 19,
+          img1: "img19A.jpeg",
+          img2: "img19B.jpeg",
+          title: "Ice Tongue Fracture and Loss, Thwaites Glacier, West Antarctica",
+          description: "A thick mass of floating ice, called an \"ice tongue\" stretches from Thwaites Glacier, which sits on solid ground in West Antarctica, into the Amundsen Sea. These images show the fracturing and loss of much of the ice tongue from 2001 to 2019. Ice moving from land to sea contributes to sea-level rise, and the amount of ice flowing into the sea from Thwaites has doubled in the span of three decades."
+        },
+        {
+          id: 20,
+          img1: "img20A.jpeg",
+          img2: "img20B.jpeg",
+          title: "Hurricane Laura Floods Southwestern Louisiana",
+          description: "Hurricane Laura made landfall the night of Aug. 26, 2020, as a Category 4 storm. It battered southwestern Louisiana with sustained winds of about 150 mph (240 kph), a catastrophic storm surge from the Gulf of Mexico, and heavy rain. The Aug. 29 image shows flooded areas around the towns of Cameron and Creole. The images are displayed in false color that helps to distinguish flooded areas (navy blue and black) from drier ground. Vegetation appears bright green and clouds are cyan."
+        },
+        {
+          id: 21,
+          img1: "img21A.jpeg",
+          img2: "img21B.jpeg",
+          title: "Southeastern Australia Turns Green",
+          description: "Much of this region in southeastern Australia suffered its driest three years on record, according to the country's Bureau of Meteorology, until heavy rains brought relief in early 2020."
+        },
+        {
+          id: 22,
+          img1: "img22A.jpeg",
+          img2: "img22B.jpeg",
+          title: "Spring Greening in Taklamakan Desert, China",
+          description: "Taklamakan is China's hottest, driest and largest desert. In the local Ugyur language, the name roughly translates to, “You go in, but you won’t come out.” But even in this arid place, springtime brings plant life. These images show a portion of the Taklamakan Desert near the oasis town of Hotan before and after sizable areas sprouted vegetation, watered to a large extent by runoff from the Kunlun Mountains (bottom)."
+        },
+        {
+          id: 23,
+          img1: "img23A.jpeg",
+          img2: "img23B.jpeg",
+          title: "Recovery of Mount St. Helens, Washington",
+          description: "Mount St. Helens erupted violently in 1980, destroying hundreds of square miles of area forest. Four years later, the affected landscape was still largely barren, as the 1984 image shows. But four decades after the blast, the region had enjoyed a great deal of recovery, as seen in the 2020 image. The large white areas are snow."
+        },
+        {
+          id: 24,
+          img1: "img24A.jpeg",
+          img2: "img24B.jpeg",
+          title: "James River Floods in South Dakota",
+          description: "These images show a portion of the James River in eastern South Dakota. The 2015 image depicts the river in a typical spring, while in the 2020 image, it is overflowing its banks. This and other sections of the river had been at flood level since spring 2019. (See Flooding Limits Planting in South Dakota.) In these false-color images, ice appears light blue and water is dark blue. The blue area merging with the James from below is Putney Slough, which also flooded."
+        },
+        {
+          id: 25,
+          img1: "img25A.jpeg",
+          img2: "img25B.jpeg",
+          title: "Travel Reduced Around Wuhan, China",
+          description: "The bright areas in these nighttime images are the lights of Wuhan, China, and surrounding population centers. The threads of light connecting these areas in the January image are highways, made visible by cars and trucks. Most of the highways disappear in the February image, taken after Chinese authorities suspended travel in an effort to contain the novel coronavirus."
+        },
       ],
     };
   },
