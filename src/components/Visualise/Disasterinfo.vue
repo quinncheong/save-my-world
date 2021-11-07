15f7581675143716dc7b511d6d072e5c25b0b9dd
Wong Jie Peng <76239879+jiepengwong@users.noreply.github.com>
2021-11-08 00:47:31 +0800

    Font awesome icons

--- src/components/Visualise/Disasterinfo.vue
+++ src/components/Visualise/Disasterinfo.vue
@@ -1,55 +1,118 @@
 <template>
   <section class="disaster-wrapper">
-    <div class="disaster-info-top bg-light text-dark">
-        <h1 class="display-5 text-start fw-bold">Disasters</h1>
+    <section class="disasterinfo">
+      <div class="disaster-info-top bg-light text-dark">
+        <div class="container">
+          <header class="section-header">
+            <h3>Disasters</h3>
+          </header>
 
-        <div class="row">
-        <p class="col-md-8 mt-3 text-start fs-5">
-          Climate change affects global temperature and precipitation patterns.
-          These effects, in turn, influence the intensity and, in some cases,
-          the frequency of extreme environmental events, such as forest fires,
-          hurricanes, heat waves, floods, droughts, and storms.
-        </p>
+          <div class="row">
+            <div class="col-lg-6 text-start mt-3" >
+              <h6 class="textformat">
+                Climate change affects global temperature and precipitation
+                patterns. These are some of the listed effects:
+              </h6>
 
-        <p class='col-md-4 mt-3'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis, libero.</p>
+              <!-- icon box -->
+              <div class="icon-box p-2 d-flex justify-content-start" v-for="icon,disaster in disasterShow" :key="disaster">
 
+                <div class="icon p-2border border-dark p-2 rounded-circle me-2">
+                  <!-- <font-awesome-icon icon="circle" ></font-awesome-icon> -->
+                  <font-awesome-icon :icon=icon size="lg" ></font-awesome-icon>
+                </div>
+
+                <p class="mt-2 ms-2 fw-bolder textformat" >{{disaster}}</p>
+
+              </div>
+            </div>
+
+            <div class="col">
+              <img src="@/assets/img/NaturalDisaster.webp" class="img-fluid mt-5">
+
+            </div>
+          </div>
         </div>
-    </div>
 
-    <div class="disaster-content p-5 container-fluid bg-success w-100">
-      <h1 class="mb-3">Reduce impact of disasters</h1>
-      <h6 class="mb-4">
-        Here are ways where <span id="savemyworld">YOU</span> can reduce the effects of natural disasters
-         
-      </h6>
+        <!-- <div class="row">
+          <p class="col-md-12 mt-3 text-center fs-5">
+            Climate change affects global temperature and precipitation
+            patterns. These are some of the listed effects: 1) Forest fires 2)
+            hurricanes 3) Heat waves 4) Floods 5) Droughts 6) Storms
+          </p>
+        </div> -->
 
-      <h6 class="mb-2">
-        Hence, play your part in limiting the effects of global warming and take
-        care of our planet.
-        <span id="savemyworld">Make the right choices today.</span> You can be
-        part of the solution and influence change.
-      </h6>
-    </div>
+        <!-- Top 5 disasters of all time (Time line perhaps) -->
+      </div>
+    </section>
+
+    <!-- Top disasters occurence -->
+
+    <section class="top-disaster">
+      <div class="disaster-content p-5 container-fluid bg-success w-100">
+        <h1 class="mb-3">Reduce impact of disasters</h1>
+        <h6 class="mb-4">
+          Here are ways where <span id="savemyworld">YOU</span> can reduce the
+          effects of natural disasters
+        </h6>
+
+        <h6 class="mb-2">
+          Hence, play your part in limiting the effects of global warming and
+          take care of our planet.
+          <span id="savemyworld">Make the right choices today.</span> You can be
+          part of the solution and influence change.
+        </h6>
+      </div>
+    </section>
   </section>
 </template>
 
 <script>
 export default {
   name: "Disasterinfo",
+
+  data() {
+    return {
+      disasterShow: { 
+        "Forest fires": "fire",
+        "Hurricanes": "wind",
+        "Heat waves":"temperature-high",
+        "Floods":"water",
+        "Droughts": "tint-slash",
+        "Storms":"poo-storm",
+      },
+
+    };
+  },
 };
 </script>
 
 <style lang="scss" scoped>
-.disaster-wrapper{
+.disaster-wrapper {
   font-size: $variable-font-small;
   width: 100%;
   display: flex;
   flex-direction: column;
   margin-top: 1rem;
 
-  .disaster-info-top{
+  .disaster-info-top {
     padding: 1rem;
     border-radius: 10px;
+
+  }
+
+  .textformat{
+  font-size: 20px;
+  }
+
+  .icon{
+      border-radius: 100%;
+        justify-content: center;
+
+
   }
 }
+
+
+
 </style>
