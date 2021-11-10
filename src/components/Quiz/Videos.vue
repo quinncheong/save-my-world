4399f10a6c3a86549f27c47a1a04ef0524086075
asianburpgirl <estherrrlychee@gmail.com>
2021-11-11 01:07:40 +0800

    adjusted the card height

--- src/components/Quiz/Videos.vue
+++ src/components/Quiz/Videos.vue
@@ -22,66 +22,6 @@
           </div>
         </div>
       </li>
-      <!-- <li class="cards_item">
-        <div class="card bg-transparent">
-          <div class="card_image"><iframe width="100%" height="100%"
-              src="https://www.youtube.com/embed/dcBXmj1nMTQ"
-              allowfullscreen
-            ></iframe></div>
-          <div class="card_content">
-            <h6 class="card_title">What is Climate Change?</h6>
-            <p class="card_text">The hard facts about global warming - a defining issue of our time. Climate change is happening now, and even world leaders meeting for climate talks at this year’s Cop25 know we aren’t doing enough to stop it.</p>
-          </div>
-        </div>
-      </li>
-      <li class="cards_item">
-        <div class="card bg-transparent">
-          <div class="card_image"><iframe width="100%" height="100%"
-              src="https://www.youtube.com/embed/0Puv0Pss33M"
-              allowfullscreen
-            ></iframe></div>
-          <div class="card_content">
-            <h6 class="card_title">How to Save Our Planet</h6>
-            <p class="card_text">It’s simple. Sir David Attenborough explains how humans can take charge of our future and save our planet.</p>
-          </div>
-        </div>
-      </li>
-      <li class="cards_item">
-        <div class="card bg-transparent">
-          <div class="card_image"><iframe width="100%" height="100%"
-              src="https://www.youtube.com/embed/aNX__544IO0"
-              allowfullscreen
-            ></iframe></div>
-          <div class="card_content">
-            <h6 class="card_title">Queen Elizabeth: Time for action on climate change</h6>
-            <p class="card_text">Britain's Queen Elizabeth told the United Nations climate change summit on Monday that "the time for words has now moved to the time for action", as she urged world leaders to think of future generations when negotiating a deal to limit global warming</p>
-          </div>
-        </div>
-      </li>
-      <li class="cards_item">
-        <div class="card bg-transparent">
-          <div class="card_image"><iframe width="100%" height="100%"
-              src="https://www.youtube.com/embed/SN5-DnOHQmE"
-              allowfullscreen
-            ></iframe></div>
-          <div class="card_content">
-            <h6 class="card_title">What Is the Greenhouse Effect?</h6>
-            <p class="card_text">Earth is a comfortable place for living things. It’s just the right temperatures for plants and animals – including humans – to thrive. Why is Earth so special?</p>
-          </div>
-        </div>
-      </li>
-      <li class="cards_item">
-        <div class="card bg-transparent">
-          <div class="card_image"><iframe width="100%" height="100%"
-              src="https://www.youtube.com/embed/fu5lwMissg0"
-              allowfullscreen
-            ></iframe></div>
-          <div class="card_content">
-            <h6 class="card_title">How melting Arctic ice could cause uncontrollable climate change</h6>
-            <p class="card_text">Arctic scientists have reported that the speed at which the northern ice cap is melting risks triggering 19 climate tipping points, with disastrous consequences.</p>
-          </div>
-        </div>
-      </li> -->
     </ul>
   </div>
 </template>
@@ -168,7 +108,6 @@ export default {
     display: flex;
     padding: 1rem;
     transition: all 0.3s ease;
-      
   }
 
   .cards_item:hover {
@@ -204,6 +143,7 @@ export default {
       rgba(80, 167, 194, 0.9),
       rgba(144, 243, 199, 0.9)
     );
+    height: 18rem;
   }
 
   .card_title {
