afe9874ee18be0e55707e4e76b6826718857b22c
asianburpgirl <estherrrlychee@gmail.com>
2021-11-11 11:39:56 +0800

    just changed renamed the folder

---
+++ src/components/Learning/News.vue
@@ -0,0 +1,80 @@
+<template>
+  <!-- header -->
+  <div class="news-change-wrapper">
+    <h5 class="mb-3" style="text-align: left">
+      <strong>Overview: Weather, Global Warming and Climate Change</strong>
+    </h5>
+    <div class="card bg-transparent text-white">
+      <img
+        class="card-img"
+        src="https://climate.nasa.gov/system/content_pages/main_images/1321_cc-vs-gw-vs-wx-768px.jpg"
+        alt="Card image"
+      />
+      <p style="font-weight: light">
+        “Climate change” and “global warming” are often used interchangeably but
+        have distinct meanings. Similarly, the terms "weather" and "climate" are
+        sometimes confused, though they refer to events with broadly different
+        spatial- and timescales.
+      </p>
+    </div>
+    <div class="row">
+      <div 
+        :key="index"
+        v-for="(srcset, index) of sources"
+        class="col-md-4">
+        <div class="card my-2 bg-transparent border-light">
+          <p class="border-bottom text-center" style="color: yellow">{{ srcset.title }}</p>
+          <div class="card-body">
+            <p class="card-text text-center">
+              {{ srcset.text }}
+            </p>
+            <p class="mb-1 text-center">
+              <a
+                class="buttonText text-success badge border py-1"
+                href="https://www.nature.com/articles/d41586-021-02990-w"
+                >{{ srcset.buttonText }}</a
+              >
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: "News",
+  components: {},
+  data() {
+    return {
+      sources: [
+        {
+          title: "Climate Change",
+          text: "Contemporary climate change includes both global warming caused by humans and its impacts on Earth's weather patterns.",
+          buttonText: "Click for more info"
+        },
+        {
+          title: "Greenhouse Gases",
+          text: "The main gases responsible for the greenhouse effect include carbon dioxide, methane, nitrous oxide, water vapor and fluorinated gases.",
+          buttonText: "Click for more info"
+        },
+        {
+          title: "Ways to contribute",
+          text: "Foster innovation and resilient infrastructure, creating communities and cities able to produce and consume sustainably.",
+          buttonText: "Click for more info"
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+  p {
+    text-align: left;
+    font-size: 15px;
+    padding: 10px;
+  }
+</style>
---
+++ src/views/Learning.vue
@@ -0,0 +1,85 @@
+<template>
+  <div class="quiz-wrapper">
+    <div v-if="!started">
+      <div class="mb-3">
+        <h4 class="mb-2"><strong>Quiz Time</strong></h4>
+        <!-- <font-awesome-icon icon="fa-solid fa-comments-question-check" /> -->
+        <p class="mb-2" style="font-size: 15px">
+          
+          <i
+            >Challenge yourself with our quiz! Answer all the questions to the
+            best of your capabilities! May the force be with you.
+            If you need some preparation, feel free to <b>SCROLL</b> down to look through the articles and videos provided by our team
+            </i
+          >
+        </p>
+        <button class="quizBtn btn btn-light my-2" @click="startQuiz()">
+          Start Quiz
+        </button>
+      </div>
+      <hr />
+      <News />
+      <hr />
+      <Video />
+    </div>
+    <!-- Only render the quiz if they click the start button -->
+    <Quizzes :quizzes="quizzes" v-else />
+  </div>
+</template>
+
+<script>
+import Quizzes from "@/components/Quiz/Quizzes.vue";
+import Header from "@/components/Quiz/Header.vue";
+import Video from "@/components/Quiz/Videos.vue";
+import News from "@/components/Quiz/News.vue";
+
+import { getQuizzes } from "@/firebase.js";
+
+export default {
+  name: "Community",
+  components: {
+    Quizzes,
+    Header,
+    Video,
+    News,
+  },
+  data() {
+    return {
+      quizzes: [],
+      started: false,
+    };
+  },
+  methods: {
+    async startQuiz() {
+      // Generate random id from 1-3 to get quiz
+      const quizId = Math.floor(Math.random() * 3) + 1;
+      const quizzes = await getQuizzes(quizId);
+      this.quizzes = quizzes;
+      this.started = true;
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+// display 70% width on desktop
+@media screen and (min-width: 768px) {
+  .quiz-wrapper {
+    width: 90%;
+  }
+}
+
+// display 70% width on large screen sizes
+@media screen and (min-width: 992px) {
+  .quiz-wrapper {
+    width: 80%;
+  }
+}
+
+// // display 70% width on extra large screen sizes
+@media screen and (min-width: 1200px) {
+  .quiz-wrapper {
+    width: 60%;
+  }
+}
+</style>
