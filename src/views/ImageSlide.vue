032efbd7e22d9a74a6e1b2b3ce3d50d4e63ef844
asianburpgirl <estherrrlychee@gmail.com>
2021-11-14 15:05:01 +0800

    changed the num of img to 15

--- src/views/ImageSlide.vue
+++ src/views/ImageSlide.vue
@@ -161,102 +161,108 @@ export default {
             "A slow-motion landslide on the slope that defines the western side of Barry Arm, one of the glacial fjords of Alaska's Prince William Sound, has stalled. But scientists warn that the mountainside could fail catastrophically and slide into the fjord, endangering people in the area with a tsunami hundreds of feet high. The slide appears to have begun at least 50 years ago, but sped up between 2009 and 2015 as warming temperatures shortened Barry Arm Glacier, which had supported the slope's base. The mountainside slid about 400 feet (120 meters) between 2010 and 2017, but has moved little since then.",
           timeline: "Sept. 15, 2013 - Aug. 24, 2019",
         },
-        {
-          id: 16,
-          img1: "img16A.webp",
-          img2: "img16B.webp",
-          title:
-            "Invasion of the Water Hyacinths Across Valsequillo Reservoir, Mexico",
-          description:
-            "These images show the spread of water hyacinths across the Valsequillo reservoir of central Mexico. The floating plants, native to the Amazon basin, are relentless invaders in other parts of the world. They obstruct waterways, clog hydropower plants, and block sunlight from penetrating the water’s surface. Some scientists attribute the problem to water pollution: Nutrients in untreated sewage, which often accompanies rapid urbanization, enable the plants to thrive. In these false-color images, clear water is dark blue and vegetation is red. The brighter the red, the more robust the vegetation.",
-          timeline: "Jan. 10, 2000 - Jan. 9, 2020",
-        },
-        {
-          id: 17,
-          img1: "img17A.webp",
-          img2: "img17B.webp",
-          title: "Spring in the Kulunda Steppe, Russia-Kazakhstan Border",
-          description:
-            "This portion of the Kulunda Steppe, which straddles the Russia-Kazakhstan border, displays a common springtime transformation from the white of snow and ice to the green of vegetation. Less common is Lake Kuchukskoye's change from green to pink as rosy, salt-loving organisms in the lake multiplied. Hot weather had increased evaporation and therefore the lake's salt concentration, prompting the population explosion.",
-          timeline: "April 1, 2020 - June 11, 2020",
-        },
-        {
-          id: 18,
-          img1: "img18A.webp",
-          img2: "img18B.webp",
-          title: "Sea Ice Declines in Hudson Bay",
-          description:
-            "Hudson Bay, which is considered a sea of the Arctic Ocean despite being surrounded by land, freezes over completely in winter and thaws in summer. These images show the decrease in ice over the course of 16 days in July 2020. During the thaw, polar bears travel over the slowly melting sea ice to hunt for ringed seals and other prey. When the ice becomes too scarce, the bears fast and wait for it to return. The area has lost about a third of the polar bear population since the 1980s, dropping the count from about 1200 to 800, apparently because declining summer sea ice has given them less opportunity to feed.",
-          timeline: "July 13, 2020 - July 29, 2020",
-        },
-        {
-          id: 19,
-          img1: "img19A.webp",
-          img2: "img19B.webp",
-          title:
-            "Ice Tongue Fracture and Loss, Thwaites Glacier, West Antarctica",
-          description:
-            'A thick mass of floating ice, called an "ice tongue" stretches from Thwaites Glacier, which sits on solid ground in West Antarctica, into the Amundsen Sea. These images show the fracturing and loss of much of the ice tongue from 2001 to 2019. Ice moving from land to sea contributes to sea-level rise, and the amount of ice flowing into the sea from Thwaites has doubled in the span of three decades.',
-          timeline: "Dec. 2, 2001 - Dec. 28, 2019",
-        },
-        {
-          id: 20,
-          img1: "img20A.webp",
-          img2: "img20B.webp",
-          title: "Hurricane Laura Floods Southwestern Louisiana",
-          description:
-            "Hurricane Laura made landfall the night of Aug. 26, 2020, as a Category 4 storm. It battered southwestern Louisiana with sustained winds of about 150 mph (240 kph), a catastrophic storm surge from the Gulf of Mexico, and heavy rain. The Aug. 29 image shows flooded areas around the towns of Cameron and Creole. The images are displayed in false color that helps to distinguish flooded areas (navy blue and black) from drier ground. Vegetation appears bright green and clouds are cyan.",
-          timeline: "Aug. 13, 2020 - Aug. 29, 2020",
-        },
-        {
-          id: 21,
-          img1: "img21A.webp",
-          img2: "img21B.webp",
-          title: "Southeastern Australia Turns Green",
-          description:
-            "Much of this region in southeastern Australia suffered its driest three years on record, according to the country's Bureau of Meteorology, until heavy rains brought relief in early 2020.",
-          timeline: "May 25, 2018 - June 5, 2020",
-        },
-        {
-          id: 22,
-          img1: "img22A.webp",
-          img2: "img22B.webp",
-          title: "Spring Greening in Taklamakan Desert, China",
-          description:
-            "Taklamakan is China's hottest, driest and largest desert. In the local Ugyur language, the name roughly translates to, “You go in, but you won’t come out.” But even in this arid place, springtime brings plant life. These images show a portion of the Taklamakan Desert near the oasis town of Hotan before and after sizable areas sprouted vegetation, watered to a large extent by runoff from the Kunlun Mountains (bottom).",
-          timeline: "May 25, 2018 - June 5, 2020",
-        },
-        {
-          id: 23,
-          img1: "img23A.webp",
-          img2: "img23B.webp",
-          title: "Recovery of Mount St. Helens, Washington",
-          description:
-            "Mount St. Helens erupted violently in 1980, destroying hundreds of square miles of area forest. Four years later, the affected landscape was still largely barren, as the 1984 image shows. But four decades after the blast, the region had enjoyed a great deal of recovery, as seen in the 2020 image. The large white areas are snow.",
-          timeline: "June 17, 1984 - April 17, 2020",
-        },
-        {
-          id: 24,
-          img1: "img24A.webp",
-          img2: "img24B.webp",
-          title: "James River Floods in South Dakota",
-          description:
-            "These images show a portion of the James River in eastern South Dakota. The 2015 image depicts the river in a typical spring, while in the 2020 image, it is overflowing its banks. This and other sections of the river had been at flood level since spring 2019. (See Flooding Limits Planting in South Dakota.) In these false-color images, ice appears light blue and water is dark blue. The blue area merging with the James from below is Putney Slough, which also flooded.",
-          timeline: "March 10, 2015 - March 23, 2020",
-        },
-        {
-          id: 25,
-          img1: "img25A.webp",
-          img2: "img25B.webp",
-          title: "Travel Reduced Around Wuhan, China",
-          description:
-            "The bright areas in these nighttime images are the lights of Wuhan, China, and surrounding population centers. The threads of light connecting these areas in the January image are highways, made visible by cars and trucks. Most of the highways disappear in the February image, taken after Chinese authorities suspended travel in an effort to contain the novel coronavirus.",
-          timeline: "Jan. 19, 2020 - Feb. 4, 2020",
-        },
+        // {
+        //   id: 16,
+        //   img1: "img16A.webp",
+        //   img2: "img16B.webp",
+        //   title:
+        //     "Invasion of the Water Hyacinths Across Valsequillo Reservoir, Mexico",
+        //   description:
+        //     "These images show the spread of water hyacinths across the Valsequillo reservoir of central Mexico. The floating plants, native to the Amazon basin, are relentless invaders in other parts of the world. They obstruct waterways, clog hydropower plants, and block sunlight from penetrating the water’s surface. Some scientists attribute the problem to water pollution: Nutrients in untreated sewage, which often accompanies rapid urbanization, enable the plants to thrive. In these false-color images, clear water is dark blue and vegetation is red. The brighter the red, the more robust the vegetation.",
+        //   timeline: "Jan. 10, 2000 - Jan. 9, 2020",
+        // },
+        // {
+        //   id: 17,
+        //   img1: "img17A.webp",
+        //   img2: "img17B.webp",
+        //   title: "Spring in the Kulunda Steppe, Russia-Kazakhstan Border",
+        //   description:
+        //     "This portion of the Kulunda Steppe, which straddles the Russia-Kazakhstan border, displays a common springtime transformation from the white of snow and ice to the green of vegetation. Less common is Lake Kuchukskoye's change from green to pink as rosy, salt-loving organisms in the lake multiplied. Hot weather had increased evaporation and therefore the lake's salt concentration, prompting the population explosion.",
+        //   timeline: "April 1, 2020 - June 11, 2020",
+        // },
+        // {
+        //   id: 18,
+        //   img1: "img18A.webp",
+        //   img2: "img18B.webp",
+        //   title: "Sea Ice Declines in Hudson Bay",
+        //   description:
+        //     "Hudson Bay, which is considered a sea of the Arctic Ocean despite being surrounded by land, freezes over completely in winter and thaws in summer. These images show the decrease in ice over the course of 16 days in July 2020. During the thaw, polar bears travel over the slowly melting sea ice to hunt for ringed seals and other prey. When the ice becomes too scarce, the bears fast and wait for it to return. The area has lost about a third of the polar bear population since the 1980s, dropping the count from about 1200 to 800, apparently because declining summer sea ice has given them less opportunity to feed.",
+        //   timeline: "July 13, 2020 - July 29, 2020",
+        // },
+        // {
+        //   id: 19,
+        //   img1: "img19A.webp",
+        //   img2: "img19B.webp",
+        //   title:
+        //     "Ice Tongue Fracture and Loss, Thwaites Glacier, West Antarctica",
+        //   description:
+        //     'A thick mass of floating ice, called an "ice tongue" stretches from Thwaites Glacier, which sits on solid ground in West Antarctica, into the Amundsen Sea. These images show the fracturing and loss of much of the ice tongue from 2001 to 2019. Ice moving from land to sea contributes to sea-level rise, and the amount of ice flowing into the sea from Thwaites has doubled in the span of three decades.',
+        //   timeline: "Dec. 2, 2001 - Dec. 28, 2019",
+        // },
+        // {
+        //   id: 20,
+        //   img1: "img20A.webp",
+        //   img2: "img20B.webp",
+        //   title: "Hurricane Laura Floods Southwestern Louisiana",
+        //   description:
+        //     "Hurricane Laura made landfall the night of Aug. 26, 2020, as a Category 4 storm. It battered southwestern Louisiana with sustained winds of about 150 mph (240 kph), a catastrophic storm surge from the Gulf of Mexico, and heavy rain. The Aug. 29 image shows flooded areas around the towns of Cameron and Creole. The images are displayed in false color that helps to distinguish flooded areas (navy blue and black) from drier ground. Vegetation appears bright green and clouds are cyan.",
+        //   timeline: "Aug. 13, 2020 - Aug. 29, 2020",
+        // },
+        // {
+        //   id: 21,
+        //   img1: "img21A.webp",
+        //   img2: "img21B.webp",
+        //   title: "Southeastern Australia Turns Green",
+        //   description:
+        //     "Much of this region in southeastern Australia suffered its driest three years on record, according to the country's Bureau of Meteorology, until heavy rains brought relief in early 2020.",
+        //   timeline: "May 25, 2018 - June 5, 2020",
+        // },
+        // {
+        //   id: 22,
+        //   img1: "img22A.webp",
+        //   img2: "img22B.webp",
+        //   title: "Spring Greening in Taklamakan Desert, China",
+        //   description:
+        //     "Taklamakan is China's hottest, driest and largest desert. In the local Ugyur language, the name roughly translates to, “You go in, but you won’t come out.” But even in this arid place, springtime brings plant life. These images show a portion of the Taklamakan Desert near the oasis town of Hotan before and after sizable areas sprouted vegetation, watered to a large extent by runoff from the Kunlun Mountains (bottom).",
+        //   timeline: "May 25, 2018 - June 5, 2020",
+        // },
+        // {
+        //   id: 23,
+        //   img1: "img23A.webp",
+        //   img2: "img23B.webp",
+        //   title: "Recovery of Mount St. Helens, Washington",
+        //   description:
+        //     "Mount St. Helens erupted violently in 1980, destroying hundreds of square miles of area forest. Four years later, the affected landscape was still largely barren, as the 1984 image shows. But four decades after the blast, the region had enjoyed a great deal of recovery, as seen in the 2020 image. The large white areas are snow.",
+        //   timeline: "June 17, 1984 - April 17, 2020",
+        // },
+        // {
+        //   id: 24,
+        //   img1: "img24A.webp",
+        //   img2: "img24B.webp",
+        //   title: "James River Floods in South Dakota",
+        //   description:
+        //     "These images show a portion of the James River in eastern South Dakota. The 2015 image depicts the river in a typical spring, while in the 2020 image, it is overflowing its banks. This and other sections of the river had been at flood level since spring 2019. (See Flooding Limits Planting in South Dakota.) In these false-color images, ice appears light blue and water is dark blue. The blue area merging with the James from below is Putney Slough, which also flooded.",
+        //   timeline: "March 10, 2015 - March 23, 2020",
+        // },
+        // {
+        //   id: 25,
+        //   img1: "img25A.webp",
+        //   img2: "img25B.webp",
+        //   title: "Travel Reduced Around Wuhan, China",
+        //   description:
+        //     "The bright areas in these nighttime images are the lights of Wuhan, China, and surrounding population centers. The threads of light connecting these areas in the January image are highways, made visible by cars and trucks. Most of the highways disappear in the February image, taken after Chinese authorities suspended travel in an effort to contain the novel coronavirus.",
+        //   timeline: "Jan. 19, 2020 - Feb. 4, 2020",
+        // },
       ],
     };
   },
 };
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+  @media screen and (min-width: 320px) {
+  .quiz-wrapper {
+    width: 90%;
+  }
+}
+</style>
